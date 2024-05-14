0b873723e890cd51eca51d8fee338fe14b0cf148
NZ <n.prokopyk@gmail.com>
2024-05-14 12:37:32 +0200

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import React from 'react';
+import Menu from './components/Menu';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Quizz App</h1>
+      <Menu />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+export default function Menu() {
+  return (
+    <div className="Menu">
+      <input type="text" placeholder="John Smith" />
+      <button>Start Quiz</button>
+    </div>
+  );
+}
