0fd9b2923926e22a5b8ac19fbddb5faee96171c0
NZ <n.prokopyk@gmail.com>
2024-06-07 13:16:21 +0200

    Creating the Questions

---
+++ src/assets/helpers/Questions.jsx
@@ -0,0 +1,18 @@
+export const Questions = [
+  {
+    prompt: 'What is my name?',
+    optionA: 'John',
+    optionB: 'Jake',
+    optionC: 'Josh',
+    optionD: 'Pedro',
+    answer: 'optionD',
+  },
+  {
+    prompt: 'Which of this is not a programming language?',
+    optionA: 'Python',
+    optionB: 'JS',
+    optionC: 'MC-03',
+    optionD: 'Java',
+    answer: 'optionC',
+  },
+];
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Questions } from '../assets/helpers/Questions';
 import '../App.css';
 
 export default function Quiz() {
