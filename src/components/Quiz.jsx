c83ee988ff08ef35d04f3f699d5b0eb1aa4624c8
NZ <n.prokopyk@gmail.com>
2024-06-07 13:33:33 +0200

    Answering Questions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import { useState } from 'react';
 import { Questions } from '../assets/helpers/Questions';
 import '../App.css';
 
 export default function Quiz() {
-  return <div className="Quiz">Quiz</div>;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [optionChosen, setOptionChosen] = useState();
+
+  const chooseOption = (option) => {
+    setOptionChosen(option);
+  };
+  return (
+    <div className="Quiz">
+      <h1>{Questions[currentQuestion].prompt}</h1>
+      <div className="questions">
+        <button
+          onClick={() => {
+            chooseOption('optionA');
+          }}
+        >
+          {Questions[currentQuestion].optionA}
+        </button>
+        <button
+          onClick={() => {
+            chooseOption('optionB');
+          }}
+        >
+          {Questions[currentQuestion].optionB}
+        </button>
+        <button
+          onClick={() => {
+            chooseOption('optionC');
+          }}
+        >
+          {Questions[currentQuestion].optionC}
+        </button>
+        <button
+          onClick={() => {
+            chooseOption('optionD');
+          }}
+        >
+          {Questions[currentQuestion].optionD}
+        </button>
+      </div>
+      {optionChosen}
+    </div>
+  );
 }
